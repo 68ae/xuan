0669d4d21d47273271e9a310bc2fd168cc0d6d33
68ae <920444678@qq.com>
2017-04-12 15:50:15 +0800

    忽略

--- ThinkPHP/Conf/convention.php
+++ ThinkPHP/Conf/convention.php
@@ -60,10 +60,10 @@ return  array(
 
     /* 数据库设置 */
     'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  '', // 服务器地址
-    'DB_NAME'               =>  '',          // 数据库名
-    'DB_USER'               =>  '',      // 用户名
-    'DB_PWD'                =>  '',          // 密码
+    'DB_HOST'               =>  'qdm11454229.my3w.com', // 服务器地址
+    'DB_NAME'               =>  'qdm11454229_db',          // 数据库名
+    'DB_USER'               =>  'qdm11454229',      // 用户名
+    'DB_PWD'                =>  'wcx199151',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
     'DB_PREFIX'             =>  'emlog_',    // 数据库表前缀
     'DB_PARAMS'          	=>  array(), // 数据库连接参数    
